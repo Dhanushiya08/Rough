9e46be52496c55e605f8a0aa723e109d6c55ca40
Dhanushiya08 <dhanushiya@1cloudhub.com>
2026-05-08 14:17:19 +0530

    user add

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,31 +10,103 @@ const sampleUsers = [
   {
     username: "admin@1cloudhub.com",
     password:
-      "b999b3991c9e72a2af0f326fbe9cda3446e399c738ca2f600eec68c4d37b4425",
+      "1a8e2cf3d6e1e7a00cdbe5899319e2cef16c5138d30355360dc4f8540de9dbcd",
     role: "Admin",
   },
   {
     username: "user01@1cloudhub.com",
     password:
-      "f72b33d1a80b869c3c67b946f31d365e11f65cc8301695ed9fdf912030468305",
+      "951444067dc2a7ca8a889fb37ccf297e817c2c0fe237282202785225bdfbc546",
     role: "User",
   },
   {
     username: "user02@1cloudhub.com",
     password:
-      "025b5d1d8645562d412443af8628c8eee16e64b4d0967b604a92ae691514a90f",
+      "6cbd99a8a92120e95e9336a3a5880b52c11fd14e18630331bf019d014a135ed0",
     role: "User",
   },
   {
     username: "user03@1cloudhub.com",
     password:
-      "7077a1c355903de212b2c362bde670af55f682d518c36c17c82fcf7ea4571fc3",
+      "8feecc892ecd04f888e944be1c44c4ab0f5f331042fe0d219b16ce9fa9563b11",
     role: "User",
   },
   {
     username: "user04@1cloud.com",
     password:
-      "2a39c458b2d1a8ff21e1ab60c140e631e9fff4948cdb401218682a16dd242f0a",
+      "3bf30b02d7113bccc538409492d228262f0eca8bd01854b84818db8f510241b3",
+    role: "User",
+  },
+  {
+    username: "anton_hermanto@eastglobal.biz",
+    password:
+      "6f46181a8f4706c26cf7d0775b74098aad73bcd58060a87f973ff88d003ecfb3",
+    role: "User",
+  },
+  {
+    username: "fakhri_aufar@eastglobal.biz",
+    password:
+      "32d3c4b451918d603d2be22a0775691fba2729edbdf051ba77aab5a27699be65",
+    role: "User",
+  },
+  {
+    username: "margaret_limawan@eastglobal.biz",
+    password:
+      "9bea93702520e36b27216c824f8751c9f456070fff92bc763589f10cc7c37a32",
+    role: "User",
+  },
+  {
+    username: "nurkhafeezah_khalid@averis.com",
+    password:
+      "faaece25709e98ea7023955979180a84e91110b6957ff882701cb08430c78416",
+    role: "User",
+  },
+  {
+    username: "peiyin_chang@averis.com",
+    password:
+      "afeca9aebc21a07ddbb934060a1716b714489f77ae404f7022ce4668917eea98",
+    role: "User",
+  },
+  {
+    username: "carlmen_low@averis.com",
+    password:
+      "a62c63c2fdb9dda16030c909f34ea6cdfffc7a8c8593abe27846ce7aae8f7c55",
+    role: "User",
+  },
+  {
+    username: "shityiing_kok@averis.com",
+    password:
+      "925966bc9dbd7e0c68b247540d6514c25c6ac0ad0b4f66623bd5b10c0be8e2e3",
+    role: "User",
+  },
+  {
+    username: "caini_chau@averis.com",
+    password:
+      "cc8ec59bebf1be0e62b6000d36568c42a52fdc240d2fccb420504c4783bf1abf",
+    role: "User",
+  },
+  {
+    username: "hsiawen_khor@averis.com",
+    password:
+      "661861473f5c9a47b1b5b6ed5938a2184453a83333f309a4146e1b8cb81e66e2",
+    role: "User",
+  },
+  {
+    username: "yoonahnjohn_lian@averis.com",
+    password:
+      "119e0c4aa6ffd503cb29fee97839ba0b0050d1a19bfe46c77b6bfbe55a6f995b",
+    role: "User",
+  },
+  {
+    username: "kahheng_yap@pacificenergycorp.com",
+    password:
+      "e68981ed88822e35f0b8255adff6e92ad2e5b3c974ae0efef5b6e71f6c7e829f",
+    role: "User",
+  },
+  {
+    username: "peknee_chiu@averis.com",
+    password:
+      "7c9df89c7e257775d5127c59b23af1cd1e1a5535e9da47622cd7f3cb55dbf074",
     role: "User",
   },
 ];
